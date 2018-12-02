f749e4fc6a6325c4c1cfc245dcb7e309c141ecb5
Peter <pdnguyen@ryerson.ca>
2018-12-01 22:55:12 -0500

    Initial server setup: VueJS and Laravel

---
+++ resources/js/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="page1">Page 1</a></li>
+            <li><a href="page2">Page 2</a></li>
+            <li><a href="page3">Page 3</a></li>
+            <li><a href="page4">Page 4</a></li>
+            <li><a href="page5">Page 5</a></li>
+            <li><a href="page6">Page 6</a></li>
+        </ul>
+    </nav>
+</template>
+
+<style>
+    /* Removes gap from navigation bar on the page. */
+    body {
+        margin: 0px;
+    }
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover {
+        background-color: #111;
+    }
+</style>
---
+++ resources/views/page2.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>CPS 530 Project - Page 1</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div id="app">
+            <nav-bar></nav-bar>
+            Page 2
+        </div>
+        <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
