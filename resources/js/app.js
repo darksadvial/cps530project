d724821660f9aa38f304872a4cb0340a8053b232
Peter Nguyen <pdnguyen@ryerson.ca>
2018-12-02 20:41:41 -0500

    Added global style sheet.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,9 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
+// IMPORTS.
 import Vue from 'vue';
 window.Vue = require('vue');
+
+// Main/Gobal stylesheet.
+require('../css/main.css');
 
  // Bootstrap package.
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -19,27 +16,12 @@ require('./bootstrap');
 import VueImageLoader from '@kevindesousa/vue-image-loader'
 Vue.use(VueImageLoader)
 
+// Waterfall tiles.
 import {Waterfall, WaterfallItem} from 'vue2-waterfall';
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
+// Vue components.
 Vue.component('card-container', require('./components/CardContainer.vue'));
 Vue.component('nav-bar', require('./components/NavBar.vue'));
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
     el: '#app'
--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -13,7 +13,17 @@
         <div id="app">
             <!-- Navigation bar component. -->
             <nav-bar></nav-bar>
-            Page 2
+            <br>
+            <div class="container">
+                <!-- Centers body -->
+                <div class="row justify-content-center">
+                    <!-- Keeps that scale action -->
+                    <div class="col-md-12">
+                        <div class="title-header">VueJS</div>
+                        Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally adoptable. The core library is focused on the view layer only, and is easy to pick up and integrate with other libraries or existing projects. On the other hand, Vue is also perfectly capable of powering sophisticated Single-Page Applications when used in combination with modern tooling and supporting libraries.
+                    </div>
+                </div>
+            </div>
         </div>
         <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
     </body>
